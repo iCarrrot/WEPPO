57b7de288542c00f855270ea4d4556e1a5ec2d55
iCarrrot <michal.mar3@gmail.com>
2017-11-02 15:03:52 +0100

    lista 3

--- l3/z8.js
+++ l3/z8.js
@@ -13,17 +13,23 @@ function fib() {
 
 }
 
-function *fib1(){
+function* fib1() {
     var cur = 1;
     var prev = -1;
-    while(true){
+    while (true) {
         var temp = prev;
         prev = cur;
         cur += temp;
         yield cur;
     }
 }
-
+function* take(it, top) {
+    var i = 0;
+    while (i < top) {
+        i += 1;
+        yield * [it.next().value];
+    }
+}
 
 var _it = fib();
 var __i = 0;
@@ -33,10 +39,15 @@ for (var _result; _result = _it.next(), !_result.done;) {
     if (__i > 10)
         break;
 }
-__i=0
-for(var i of fib1()){
+__i = 0
+for (var i of fib1()) {
     console.log(i);
     __i += 1;
     if (__i > 10)
         break;
+}
+
+
+for (let num of take(fib1(), 20)) {
+    console.log(num);
 }
