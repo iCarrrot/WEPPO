f191b503ca55e9490bc73f49724b98f9e9ac5d19
iCarrrot <michal.mar3@gmail.com>
2017-11-21 12:12:17 +0100

    update

--- l4/z7.js
+++ l4/z7.js
@@ -14,27 +14,27 @@ lineReader.on('line', (line) => {
         dict[ip] += 1;
     }
     //print(ip, dict[ip]);
-});
-lineReader.on('close', function () {
-    var max = {}
-    for (var i = 0; i < 3; i++) {
-        var temp = {
-            ip: "",
-            c: 0
-        }
-        max[i] = Object.assign({}, temp);
-        for (var key in dict) {
-            // print(key, dict[key]);
-            if (dict[key] >= max[i].c) {
-                max[i].ip = key;
-                max[i].c = dict[key];
-            }
-        }
-        dict[max[i].ip] = 0;
-    }
-    for (var i = 0; i < 3; i++) {
-        print(max[i]);
-
-    }
-
 })
+    .on('close', function () {
+        var max = {}
+        for (var i = 0; i < 3; i++) {
+            var temp = {
+                ip: "",
+                c: 0
+            }
+            max[i] = Object.assign({}, temp);
+            for (var key in dict) {
+                // print(key, dict[key]);
+                if (dict[key] >= max[i].c) {
+                    max[i].ip = key;
+                    max[i].c = dict[key];
+                }
+            }
+            dict[max[i].ip] = 0;
+        }
+        for (var i = 0; i < 3; i++) {
+            print(max[i]);
+
+        }
+
+    })
--- l4/z8.js
+++ l4/z8.js
@@ -1,14 +1,44 @@
-fs = require('fs');
+print = console.log;
+var fs = require('fs');
+var util = require('util');
 
-fs.readFile('day.log', 'utf8', function(err, content){
- console.log('first file: ');
-//  console.log(content);
-});
+// fs.readFile('day.log', 'utf8', function(err, content){
+//  console.log('first file: ');
+// //  console.log(content);
+// });
 
-fs.readFile('z4.js', 'utf8', function(err, content){
- console.log('third file: ');
-//  console.log(content);
-});
+// fs.readFile('z4.js', 'utf8', function(err, content){
+//  console.log('third file: ');
+// //  console.log(content);
+// });
 
-console.log('koniec programu!');
-//NOT FINISHED YET!
+// console.log('koniec programu!');
+// //NOT FINISHED YET!
+
+function fspromise(path) {
+    return new Promise(function (res, rej) {
+        fs.readFile(path, function (err, data) {
+            if (err)
+                rej(err);
+            res(data.toString());
+        });
+    });
+}
+
+fspromise('day.log')
+    .then(function (data) {
+        console.log(data.length);
+    }).then(function () {
+        console.log("done");
+    })
+
+
+var readFileAsync1 = util.promisify(fs.readFile);
+
+(async function () {
+
+    var data1 = await readFileAsync1('day.log');
+    var data2 = await readFileAsync1('z1.js');
+    print(data1.length,data2.length)
+
+})();
